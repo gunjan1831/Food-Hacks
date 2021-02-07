ca87286959719c322a536de7fe4565b2727af6b4
gunjan1831 <gunjanpurwar1832000@gmail.com>
2021-02-08 01:42:35 +0530

    forms

--- Foods/Foods/urls.py
+++ Foods/Foods/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/',views.index,name='index'),
     path('<int:item_id>/',views.detail,name='detail'),
+    path('add',views.create_item,name='create_item'),
 ]
--- Foods/Info/views.py
+++ Foods/Info/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models  import Item
 from django.template import loader
+from .forms import ItemForm
 
 # Create your views here.
 def index(request):
@@ -19,3 +20,11 @@ def detail(request,item_id):
     }
 
     return render(request,'Info/detail.html',context)
+
+def create_item(request):
+    form=ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request,'Info/item-form.html',{'form':form})
